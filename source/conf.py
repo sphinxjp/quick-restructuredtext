fb5ec30992ff074f8230b9a270bcaca05bbcd875
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2021-08-29 14:21:30 +0900

    add initial

---
+++ source/conf.py
@@ -0,0 +1,8 @@
+project = 'Quick reStructuredText'
+author = 'docutils team (訳 sphinx-users.jp)'
+copyright = 'public domain (訳 sphinx-users.jp)'
+version = release = '2019-09-30 (訳 2021/08/29)'
+templates_path = '_templates/'
+root_doc = 'contents'
+html_theme = 'sphinx_rtd_theme'
+html_additional_pages = {'index': 'index.html'}
